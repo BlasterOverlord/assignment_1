3a7261d36308eddb7d63d859dd47ee062c7bbc59
BlasterOverlord <72990637+BlasterOverlord@users.noreply.github.com>
2023-11-30 22:53:52 +0600

    finished implementing the first page

--- lib/Pages/firstPage.dart
+++ lib/Pages/firstPage.dart
@@ -1,18 +1,51 @@
 import 'package:flutter/material.dart';
 
-class ProductDetails extends StatelessWidget{
+class ProductDetails extends StatelessWidget {
   const ProductDetails({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(
-        title: const Text('Product Details'),
-
-      ),
-      body: const Center(
-          child: Text('Nothing as of now...')
-      ),
+        appBar: AppBar(
+          title: const Text('Product Details'),
+        ),
+        body: Center(
+          child: Padding(
+            padding: const EdgeInsets.all(20),
+            child: Column(
+              children: [
+                Text(
+                  'Nintendo Switch',
+                  style: Theme.of(context).textTheme.headlineLarge,
+                ),
+                const SizedBox(height: 10),
+                Image.asset(
+                  'images/switch.png',
+                  height: 200,
+                  width: 200,
+                ),
+                Text(
+                  'Price: \$299.99',
+                  style: Theme.of(context).textTheme.headlineSmall?.copyWith(
+                        color: Colors.green,
+                      ),
+                ),
+                const SizedBox(height: 20),
+                Text(
+                  'Features: Versatile Gaming Modes, Joy-Con Controllers, HD Rumble Technology, Vibrant 6.2-inch Screen',
+                  style: Theme.of(context).textTheme.bodyMedium,
+                ),
+                const SizedBox(height: 30),
+                ElevatedButton(
+                  onPressed: () {
+                    // buy now
+                  },
+                  child: const Text('Buy Now'),
+                ),
+              ],
+            ),
+          ),
+        )
     );
   }
 }
