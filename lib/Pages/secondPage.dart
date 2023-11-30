9706f52c74f744c15d8ad731c81b5b112080005b
BlasterOverlord <72990637+BlasterOverlord@users.noreply.github.com>
2023-11-30 23:29:57 +0600

    added second page dart file

---
+++ lib/Pages/secondPage.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class BuyNow extends StatelessWidget {
+  const BuyNow({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
